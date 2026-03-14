4d304c859fbca9109ab4b44192b2c6eaa0ac079a
Alisson Curvina <alisson.curvina@gmail.com>
2026-03-14 17:49:02 -0300

    feat: adiciona crud de usuarios

---
+++ src/users/users.dto.ts
@@ -0,0 +1,33 @@
+import { Role } from "@prisma/client";
+import { IsDate, IsEmail, IsEnum, IsOptional, IsString } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  firstName: string
+  @IsString()
+  @IsEmail()
+  email: string
+  @IsString()
+  password: string
+  @IsString()
+  lastName: string
+  @IsString()
+  userName: string
+  @IsDate()
+  @IsOptional()
+  birthDate?: Date
+  @IsEnum(Role)
+  role?: Role = Role.USER
+}
+
+export class UpdateUserDto {
+  @IsString()
+  @IsOptional()
+  firstName?: string
+  @IsString()
+  @IsOptional()
+  lastName?: string
+  @IsDate()
+  @IsOptional()
+  birthDate?: Date
+}
