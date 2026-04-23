bd88a623b78ca03af0e217eff573524f446b3985
Natalia Taira <nataliagtaira@gmail.com>
2026-04-22 21:08:06 -0300

    feat: adiciona configuração do swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -9,8 +10,18 @@ async function bootstrap() {
 
   app.enableCors({
     origin: 'http://localhost:5173',
-    credentials: true
+    credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Urbanly API')
+    .setDescription('Endpoints da API da Urbanly, onde você pode achar seu novo lugar favorito.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
